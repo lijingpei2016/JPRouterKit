154598d92cc7c5b6bb531e19660b6570c7914604
lijingpei2016 <as330732842@qq.com>
2026-05-06 22:11:52 +0800

    Update Package.swift

--- JPRouter/Package.swift
+++ JPRouter/Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "JPRouterKit",
-            path: "Sources/JPRouter"),
+            path: "JPRouter/Sources/JPRouter"),
     ]
 )
+
